8984707d149eed94bbc4937540b557e918af028f
ThaisanUN <mr.thaisan1213@gmail.com>
2019-05-21 11:02:57 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// echo "hello world"."<br>";
+// echo "This is just Text <br>";
+// $c = 6;
+// $d = &$c;
+// echo $c;
+// echo $d;
+// $d = 9;
+// echo $c;
+// echo $d . "<br>";
+// $a = "b";
+// $b = "hello";
+// echo "<br>";
+// $hello ="this is men";
+// echo $$a;
+// echo $$b;
+$array = array(1,2,3,4,5,6,7,8,9,10);
+echo $array;
+?>
